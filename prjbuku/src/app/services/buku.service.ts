cf6d6cc17f885298c15f0afe4fd02ee23bc3b37b
citrafadilah <citrafadilahhasibuan_2226240089@mhs.mdp.ac.id>
2024-11-28 11:22:48 +0700

    Update buku.service.ts

--- prjbuku/src/app/services/buku.service.ts
+++ prjbuku/src/app/services/buku.service.ts
@@ -6,7 +6,7 @@ import { Buku } from '../models/buku.model';
   providedIn: 'root'
 })
 export class BukuService {
-  private url : string ='http://localhost:3000/bukus';
+  private url : string ='http://localhost:3000/buku';
 
   constructor(private http : HttpClient) { }
 
